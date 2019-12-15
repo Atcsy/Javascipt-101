aa0ece6986501b913a79156e19dd10ebe6794585
Attila Katona <katona.skmobil@gmail.com>
2019-12-15 23:22:48 +0100

    make fetchUser function

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,4 +1,10 @@
-//
+ async function fetchUsers() {
+     const resp = await fetch ('https://jsonplaceholder.typicode.com/users')
+     const data = await resp.json();
+     console.log(data);
+ }
+ fetchUsers();
+
 
 const urls = [
     'https://jsonplaceholder.typicode.com/users',
